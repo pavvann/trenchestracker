ca31ea2e6b391019fe73b7d93f52b3d47b0fc56d
Pawan <pawanbajaj2012@gmail.com>
2025-03-10 15:16:28 +0530

    added auth contexts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { Toaster } from "react-hot-toast"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Toaster position="top-center" />
+      <Component {...pageProps} />;
+    </AuthProvider>
+  )
 }
